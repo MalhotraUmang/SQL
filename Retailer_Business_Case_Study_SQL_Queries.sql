dd85908aa382e6a24da19d1c0416e842d01e040f
UM <umangmalhotra1980@gmail.com>
2025-02-05 00:56:57 +0530

    solved a few queries and answered a few questions

--- Retailer_Business_Case_Study_SQL_Queries.sql
+++ Retailer_Business_Case_Study_SQL_Queries.sql
@@ -1,10 +1,105 @@
 -- Questions:
 
+/*
+Q1: The first step of every RDBMS activity is to create a star schema for a particular data set. 
+You have a total of eight tables as described earlier, and 
+it is essential for you to create an entity relational diagram (ERD) in order to relate the various tables. 
+Which of the following is the most appropriate relation among ‘employees’, ‘customers’, ‘orders’ and ‘offices’ based on the
+problem statement?
+
+-- A customer can be handled by one and only one employee as their sales representative.
+-- An employee might be handling one or many customers as their sales representative.
+
+-- An order can be placed by one and only one customer.
+-- A customer can place one or more orders.
+
+-- A customer can pay with one or more cheques
+-- A payment can be done by one and only one customer
+
+-- An employee can be part of one and only one office
+-- An office can have one or more employees
+
+A: Option B
+
+The 'customers' table has only a one-to-many/zero relation with the 'orders' table. 
+The 'employees' table has one/zero to many/zero relation with the 'customers' table. 
+Also, the 'offices' table has only a one-to-many/zero relation with the 'employees' table.
+*/
+
+
+/*
+Q2: Which of the following table names has a self-referential relationship based on the descriptions
+of columns of each table given in the problem statement?
+
+A: Option C: employees
+
+The 'employees' table has a field 'reportsTo' which indicates that some of the employees have other employees as their reporting managers.
+So, this table should have a self referencial relationship.
+*/
+
+/*
+Q3: As you can see in the figure below, a one/zero-to-many/zero relation exists between the 'employees' and 'customers' tables. 
+Identify the foreign key in the 'customers' table that relates this table with the 'employees' table.
+
+A: Option D: salesRepEmployeeNumber
+
+An employee is expected to be associated to zero or more customers as a sales representative.
+*/
+
+/*
+Q4: Match the following actions with the appropriate SQL commands presented in the table given below.
+
+a | Find all the employees from the 'employees' table | I   | ALTER
+b | Add information about a new employee              | II  | DROP
+c | Change the structure of the employee table        | III | SELECT
+d | Remove the 'employees' table                      | IV  | INSERT
+
+A: Option D: a-III, b-IV, c-I, d-II
+*/
+
+/*
+Q5: Suppose you want to create a table named 'payments' in SQL with the two primary keys
+'customerNumber' and 'checkNumber', as mentioned in the ERD. 
+Which of the following queries is the correct one to achieve this?
+
+A: Option A: 
+
+CREATE TABLE `payments` (
+  `customerNumber` int(11) NOT NULL,
+  `checkNumber` varchar(12) NOT NULL,
+  `paymentDate` datetime DEFAULT NULL,
+  `amount` decimal(12,2) DEFAULT NULL,
+  PRIMARY KEY (`customerNumber`,`checkNumber`),
+  CONSTRAINT `customerNumber_fk` FOREIGN KEY (`customerNumber`) REFERENCES `customers` (`customerNumber`)
+)
+
+A composite primary key can be defined with both the key fields mentioned at the same time.
+*/
+
 -- Q-6: Insert the following two rows in the 'customers' table.
 -- (495,'Diecast Collectables','Franco','Valarie','Boston','MA','51003','USA','1188',85100),
 -- (496,'Kelly\'s Gift Shop','Snowden','Tony','Auckland  ','NULL','NULL','New Zealand','1612',110000)
 
+# check number of records in customers table first
+SELECT COUNT(*) FROM customers;
+# 120 records
 
+# insert the new records
+INSERT INTO customers
+VALUES
+(495,'Diecast Collectables','Franco','Valarie','Boston','MA','51003','USA','1188',85100),
+(496,'Kelly\'s Gift Shop','Snowden','Tony','Auckland','NULL','NULL','New Zealand','1612',110000);
+# 2 records added
+
+# check number of records in customers table again to verify
+SELECT COUNT(*) FROM customers;
+# 122 records
+
+# confirm again by looking at unique customers in customers table
+SELECT COUNT(DISTINCT customerNumber) FROM customers;
+# 122 unique customers
+
+-- A: Option A: INSERT
 
 
 -- Q-7: In the "employees" table there are some entries where 'SR' is written instead of 'Sales Rep' where office code is equal to 4.
